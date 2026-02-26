77f5810dd6e93753044387bae7f415fcf1b2febd
NahunE <myjsh6547@naver.com>
2026-02-26 09:48:02 +0900

    [Feat] 현재 로그인 한 사용자 정보 받아오기 JwtFilter에서 AuthUserDetails에 사용자 정보를 담아주면
    컨트롤러의 매개변수에서 @AuthenticationPrincipal AuthUserDetails user 코드로 사용자 객체를 받아올 수 있다.

--- src/main/java/com/example/demo/reply/ReplyController.java
+++ src/main/java/com/example/demo/reply/ReplyController.java
@@ -3,8 +3,10 @@ package com.example.demo.reply;
 import com.example.demo.common.model.BaseResponse;
 import com.example.demo.relation.model.B;
 import com.example.demo.reply.model.ReplyDto;
+import com.example.demo.user.model.AuthUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,11 +17,18 @@ import java.util.List;
 public class ReplyController {
     private final ReplyService replyService;
 
-    @PostMapping("/write")
-    public ResponseEntity write(@RequestBody ReplyDto.Req dto) {
-        replyService.register(dto);
-        return ResponseEntity.ok(BaseResponse.success("성공"));
+    @PostMapping("/reg/{boardIdx}")
+    public ResponseEntity reg(
+            @AuthenticationPrincipal AuthUserDetails user,
+            @PathVariable Long boardIdx) {
+
+        System.out.println(user);
+
+        return ResponseEntity.ok(
+                BaseResponse.success("성공")
+        );
     }
+
     @GetMapping("/list")
     public ResponseEntity list() {
         List<ReplyDto.ListRes> dto = replyService.list();
